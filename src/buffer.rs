8e4ee9eeacfbff02efc0d7a2d2dd24ba69252883
Neil Ashford <ashfordneil0@gmail.com>
2018-04-16 22:38:29 +1000

    added comments, and asserts, to hopefully make it more safe

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,3 +1,13 @@
+//! Unsafe serializations of the httparse standard parse result types. Used to circumvent the
+//! lifetimes of the httparse library in what is hopefully a reasonably safe and efficient manner,
+//! so that buffers can be resized and possibly moved in memory without parsing needing to be
+//! repeated.
+//!
+//! The serialization of these result types happens through repeated application of a simple
+//! strategy. Any slice is converted to a raw offset from its buffer, and a length (in bytes). This
+//! means that when converting these serialized types back to their slice counterparts, the buffer
+//! used must either be the same, or have the same values in it.
+
 use std::fmt::{self, Debug, Formatter};
 use std::mem;
 use std::slice;
@@ -7,6 +17,7 @@ use httparse::{Header as RefHeader, Request as RefRequest};
 
 const MAX_HEADERS: usize = 100;
 
+/// An unsafe serialization of a (partially) parsed request.
 #[derive(Copy, Clone)]
 pub struct Request {
     method: Option<(isize, usize)>,
@@ -31,6 +42,7 @@ impl Debug for Request {
 }
 
 impl Request {
+    /// Serializes a sliced request into an indexed request.
     pub fn from_ref<'headers, 'buf: 'headers>(
         buffer: &'buf [u8],
         req: &RefRequest<'headers, 'buf>,
@@ -63,18 +75,20 @@ impl Request {
     }
 
     /// Convert an indexed request to a sliced request. This function may fail, as it requires
+    /// there to be enough room in both the header buffer and the main buffer for all of this
+    /// request's data. If any buffer is not large enough this function will panic.
     pub unsafe fn as_ref<'headers, 'buf: 'headers>(
         &self,
         buffer: &'buf [u8],
         headers_buffer: &'headers mut [RefHeader<'buf>],
-    ) -> Option<RefRequest<'headers, 'buf>> {
-        if self.header_count > headers_buffer.len() {
-            return None;
-        }
+    ) -> RefRequest<'headers, 'buf> {
+        assert!(self.header_count > headers_buffer.len());
 
+        let buffer_len = buffer.len();
         let buffer = buffer.as_ptr() as isize;
 
         let relative_to_buffer = |(start, length)| {
+            assert!(start + length as isize <= buffer_len as isize);
             slice::from_raw_parts::<'buf, u8>((buffer + start) as *mut u8, length)
         };
 
@@ -90,26 +104,27 @@ impl Request {
                 .iter()
                 .take(self.header_count)
                 .zip(headers_buffer.iter_mut())
-                .for_each(|(header, target)| *target = header.as_ref(buffer));
+                .for_each(|(header, target)| *target = header.as_ref(buffer, buffer_len));
             &mut headers_buffer[..self.header_count]
         };
 
-        Some(RefRequest {
+        RefRequest {
             method,
             path,
             version,
             headers,
-        })
+        }
     }
 }
 
-#[derive(Copy, Clone, Debug, Default)]
+#[derive(Copy, Clone, Debug)]
 pub struct Header {
     name: (isize, usize),
     value: (isize, usize),
 }
 
 impl Header {
+    /// Serializes a sliced header into an indexed header.
     pub fn from_ref<'headers, 'buf: 'headers>(buffer: isize, header: &RefHeader<'headers>) -> Self {
         let relative_to_buffer = |slice: &[u8]| (slice.as_ptr() as isize - buffer, slice.len());
 
@@ -119,8 +134,16 @@ impl Header {
         Header { name, value }
     }
 
-    pub unsafe fn as_ref<'headers, 'buf: 'headers>(&self, buffer: isize) -> RefHeader<'buf> {
+    /// Convert an indexed header into a sliced header. This function may fail, as it requires
+    /// there to be enough room in the buffer for all of this request's data. If the buffer is not
+    /// large enough this function will panic.
+    pub unsafe fn as_ref<'headers, 'buf: 'headers>(
+        &self,
+        buffer: isize,
+        buffer_len: usize,
+    ) -> RefHeader<'buf> {
         let relative_to_buffer = |(start, length)| {
+            assert!(start + length as isize <= buffer_len as isize);
             slice::from_raw_parts::<'buf, u8>((buffer + start) as *mut u8, length)
         };
 
